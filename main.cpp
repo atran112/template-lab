14de19f4d16f582969b7b2fda18a4fe830ed83ad
atran112 <53277408+atran112@users.noreply.github.com>
2019-08-05 16:18:28 +0100

    Turned the fizzbuzz algorithm into a function (#2)
    Functionalize the fizzbuzz algorithm

--- main.cpp
+++ main.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 using namespace std;
 
+void fizzbuzz(unsigned int);
+
 int main () {
-	for (unsigned i = 0; i < 101; ++i) {
+
+	unsigned int max_val = 101;
+	fizzbuzz(max_val);
+}
+
+void fizzbuzz(unsigned int max_val) {
+
+	for (unsigned i = 0; i < max_val; ++i) {
         	if (i % 4 == 0 && i % 6 == 0) {
-  	        	cout << "fizzbuzz" << endl;
+  	          cout << "fizzbuzz" << endl;
        		}
         	else if (i % 4 == 0) {
-            		cout << "fizz" << endl;
+            	cout << "fizz" << endl;
         	}
         	else if (i % 6 == 0) {
-            		cout << "buzz" << endl;
+            	cout << "buzz" << endl;
         	}
         	else {
-            		cout << i << endl;
+            	cout << i << endl;
 		}
 	}        	
 }
