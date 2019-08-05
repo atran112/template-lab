e7c4091718066d81e606046bde8b2406098cca0b
atran112 <53277408+atran112@users.noreply.github.com>
2019-08-04 23:56:54 -0700

    created fizzbuzz algorithm (#1)
    fizz buzz algorithm for %4 and %6

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	for (unsigned i = 0; i < 101; ++i) {
+		if (i % 4 == 0 && i % 6 == 0) {
+			cout << "fizzbuzz" << endl;
+       		}
+        	else if (i % 4 == 0) {
+            		cout << "fizz" << endl;
+        	}
+        	else if (i % 6 == 0) {
+            		cout << "buzz" << endl;
+        	}
+        	else {
+            		cout << i << endl;
+		}
+	}   		
+}
